765d9e75f1b77dbc8669bb3b00aa2bdc958a0e1f
Leandro Citro De Andrade <leandro.citro@dominos.com.br>
2022-12-07 02:36:23 -0300

    Estilização Slidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
     return (
@@ -7,12 +8,15 @@ export function Sidebar() {
             src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"/>
 
             <div className={styles.profile}>
+                <img className = {styles.avatar} src="https://avatars.githubusercontent.com/u/32752890?v=4" />
+
                 <strong> Leandro Citro </strong>
                 <span> Web Developer </span>
             </div>
 
             <footer> 
                 <a href="#"> 
+                <PencilLine size={20} />
                 Editar seu perfil </a>
             </footer>
         </aside>
