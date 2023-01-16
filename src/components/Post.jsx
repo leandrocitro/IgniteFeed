e85670f90373fcb584761b381f95faf49840d96c
Leandro Citro De Andrade <leandro.citro@dominos.com.br>
2023-01-16 06:16:18 -0300

    Delete comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -46,6 +46,15 @@ export function Post({author, publishedAt, content }) {
         //console.log(event.target.value);
     }
 
+    function deleteComment(commentToDelete) {
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment != commentToDelete;
+        })
+
+
+        setComments(commentsWithoutDeletedOne);
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -65,9 +74,9 @@ export function Post({author, publishedAt, content }) {
 <div className={styles.content}>
     {content.map(line => {
         if (line.tupe == 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
         } else if (line.type == 'link') {
-            return <p><a href="#">{line.content}</a></p>;
+            return <p key={line.content}><a href="#">{line.content}</a></p>;
         }
         
     })}
@@ -79,8 +88,8 @@ export function Post({author, publishedAt, content }) {
     name="comment"
     placeholder="Deixe um comentário"
     value={newCommentText}
-
     onChange={handleNewCommentChange}
+    required
 />
 
 <footer>
@@ -91,7 +100,7 @@ export function Post({author, publishedAt, content }) {
 
 <div className={'styles.commentList'}></div>
 {comments.map(comment => {
-    return <Comment content={comment} />
+    return <Comment key={comment} content={comment} onDeleteComment={deleteComment} />
 })}
 
 
--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -2,7 +2,13 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content , onDeleteComment}) {
+    function handleDeleteComment () {
+        console.log('Comentário deletado');
+
+        onDeleteComment(content)
+    }
+
     return (
         <div className={styles.comment}> 
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/32752890?v=4" alt="" />
@@ -15,7 +21,7 @@ export function Comment({ content }) {
                         <time title="20 de Dezembro às 20:04" dateTime="2022-05-11 20:04"> Cerca de 1 h atrás</time>
                     </div>
 
-                    <button title="Deletar comentário">
+                    <button onClick={handleDeleteComment} title="Deletar comentário">
                         <Trash size={24} />
                     </button>
 
